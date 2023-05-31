c879bff0e1ec509e492bea53a1c98106c1094283
Gopi <d.gopimanohar@gmail.com>
2023-06-01 00:47:15 +0530

    By Gopi

--- src/test/java/cucumber/Options/Runner.java
+++ src/test/java/cucumber/Options/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\java\\features\\E2E.feature",
+@CucumberOptions(features="features\\E2E.feature",
 plugin="json:target/jsonReports/cucumber-report.json",
 glue= "stepdef",
 dryRun=false)
